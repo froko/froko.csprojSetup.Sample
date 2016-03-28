1950ad3cc86ad544c7d37e55115ae23736e5d1f2
froko <github@frokonet.ch>
2016-03-28 12:54:40 +0200

    Added Octopack to build chain

--- scripts/integrate.ps1
+++ scripts/integrate.ps1
@@ -1,10 +1,11 @@
 $baseDir = Resolve-Path ..
 $scriptsDir = "$baseDir\scripts"
 $sourceDir = "$baseDir\source"
+$nugetDir = "$baseDir\nuget"
 $nugetCommand = "$sourceDir\.nuget\NuGet.exe"
 $solutionFile = "$sourceDir\froko.csprojSetup.Sample.sln"
 
 & $scriptsDir\clean.ps1	
 & $nugetCommand restore $solutionFile
 
-msbuild $solutionFile /target:Rebuild /p:Configuration=Release
+msbuild $solutionFile /target:Rebuild /p:Configuration=Release /p:RunOctoPack=true /p:OctoPackPublishPackageToFileShare=$nugetDir
